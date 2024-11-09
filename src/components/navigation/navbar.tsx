93d906406a9b9cbdc9e9f4cc2e26af45d50a7351
Jiya Gupta <coderaholic21@gmail.com>
2024-11-10 01:13:36 +0530

    change

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -127,7 +127,7 @@ const Navbar = () => {
                                  href="/auth/sign-up" 
                                 //  href="#waitlist" 
                                  className={buttonVariants({ size: "sm", })}>
-                                    Get Started
+                                    Get Started...
                                     <ZapIcon className="size-3.5 ml-1.5 text-orange-500 fill-orange-500" />
                                 </Link>
                             </div>
