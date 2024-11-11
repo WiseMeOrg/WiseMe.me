549afe61b4bef5ea13cca6169ff1343a9a447e1b
Jiya Gupta <coderaholic21@gmail.com>
2024-11-11 21:21:09 +0530

    fix:navlink open opactiy

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -235,8 +235,8 @@ export const SidebarLink = ({
 
         <motion.span
           animate={{
-            display: (open ? "inline-block" : "none"),
-            opacity: (open ? 1 : 0),
+            display: ("inline-block" ),
+            opacity: (1),
           }}
           className="text-neutral-700 dark:text-neutral-200 text-sm group-hover/sidebar:translate-x-1 transition duration-150 whitespace-pre inline-block !p-0 !m-0"
         >
